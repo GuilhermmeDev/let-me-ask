f072c5e779bd5d148cff5ee3723f297440630760
GuilhermmeDev <139175554+GuilhermmeDev@users.noreply.github.com>
2025-07-13 17:28:42 -0300

    ui: prototipe of landing page

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CreateRoom } from './pages/createRoom'
+import LandingPage from './pages/landingPage'
 import { RecordRoomAudio } from './pages/record-room-audio'
 import { Room } from './pages/room'
 
@@ -9,7 +10,8 @@ export function App() {
     <QueryClientProvider client={new QueryClient()}>
       <BrowserRouter>
         <Routes>
-          <Route element={<CreateRoom />} index />
+          <Route element={<LandingPage />} index />
+          <Route element={<CreateRoom />} path="/rooms" />
           <Route element={<Room />} path="/room/:roomId" />
           <Route element={<RecordRoomAudio />} path="/room/:roomId/audio" />
         </Routes>
